45efeb63c85b7b69928ee1b795073c1afb198b6e
prabhasg56 <prabhask856@gmail.com>
2023-07-30 10:47:48 +0530

    added cart data into fireBase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
-   const cartAction = useSelector((state)=>state.cart.showCart)
+  const cartAction = useSelector((state) => state.cart.showCart);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://redux-toolkit-cart-115d5-default-rtdb.firebaseio.com/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
 
   return (
     <Layout>
